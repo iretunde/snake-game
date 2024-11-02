86db83db24f983bf560bc27d0f9db743440f725f
iretunde <iretundesoleye@gmail.com>
2024-11-02 12:21:20 +0000

    Tidying up food.py to look more presentable.

--- food.py
+++ food.py
@@ -5,13 +5,15 @@ class Food(Turtle):
 
     def __init__(self):
         super().__init__()
+        
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+
         self.shape("circle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
-        random_x = random.randint(-280, 280)
-        random_y = random.randint(-280, 280)
         self.refresh()
 
     def refresh(self):
